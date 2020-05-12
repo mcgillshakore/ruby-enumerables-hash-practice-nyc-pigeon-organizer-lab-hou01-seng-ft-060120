4a579f66f775cfad6974dad87ae78c5d6613eac2
Shakore McGill <mcgills2093@gmail.com>
2020-05-12 16:25:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |characteristics, value|
+    value.each do |info, pigeon_names|
+      pigeon_names.each do |name|
+        if pigeon_list[name] == nil 
+          pigeon_list[name] = {}
+        end 
+        if pigeon_list[name][characteristics] == nil
+          pigeon_list[name][characteristics] = []
+        end
+        pigeon_list[name][characteristics].push(info.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
