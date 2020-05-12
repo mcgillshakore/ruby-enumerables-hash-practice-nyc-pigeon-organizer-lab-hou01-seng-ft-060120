4000e5ea3b694e1ada07507c7b6d3c6027ee4681
Shakore McGill <mcgills2093@gmail.com>
2020-05-12 15:42:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
+  pigeon_name = {}
   data.each do |color_gender_lives, value|
     value.each do |stats, all_names|
       all_names.each do |name|
